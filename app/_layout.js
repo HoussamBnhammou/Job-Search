6d5c66d83e75f9ccb7aa24aae7b37ae5a0729897
hossa <hossambenhammou00@gmail.com>
2023-06-24 11:43:02 +0000

    project initiation

---
+++ app/_layout.js
@@ -0,0 +1,13 @@
+import {Stack} from "expo-router"
+import { SafeAreaView, ScrollView } from "react-native";
+import {}
+
+
+const layout = () => {
+    return (
+        <Stack/ >
+    );
+}
+
+
+export default layout;
